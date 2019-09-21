88ff481738c05894edb4c28e48b23adbee638137
Eduardo Zepeda <magallon_zepeda_carlos@hotmail.com>
2019-09-21 14:51:15 -0500

    Implements redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { Fragment } from 'react'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import TodoForm from './todoForm'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <Fragment>
+      <Typography variant="h2" align="center" gutterBottom>Todo list</Typography>
+      <Grid container justify="center">
+        <Grid item>
+          <TodoForm/>
+        </Grid>
+      </Grid>
+      <Grid container justify="center">
+        <Grid item md={8}>
+          {/* <ListItems
+            todos = {this.state.todos}
+            toggleCompleted = {this.toggleCompleted}
+            deleteTodo = {this.deleteTodo}
+          /> */}
+        </Grid>
+      </Grid>
+    </Fragment>
   );
 }
 
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,6 +1,6 @@
 import { UPDATE_VALUE } from './actionTypes'
 
-export const updateValue = () => {
+export const updateValue = (value) => {
   return {
     type: UPDATE_VALUE,
     payload: value
